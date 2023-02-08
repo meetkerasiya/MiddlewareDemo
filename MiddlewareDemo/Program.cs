869bcb03e01feec7f218517c1bc56113198e66d4
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-08 15:33:38 +0530

    Use and Map methods added

--- MiddlewareDemo/Program.cs
+++ MiddlewareDemo/Program.cs
@@ -12,12 +12,37 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if(app.Environment.IsDevelopment())
 {
+    app.Use(async (context, next) =>
+    {
+        await context.Response.WriteAsync($"Before Request {Environment.NewLine}");
+        await next();
+        await context.Response.WriteAsync($"After Request {Environment.NewLine}");
+    });
+    app.Map("/first", FirstBranch);
+    app.Map("/second", SecondBranch);
     app.Run(async context =>
     {
-       await context.Response.WriteAsync("Hello Readers!!");
+       await context.Response.WriteAsync($"Hello Readers!! {Environment.NewLine}");
     });
+    
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+void SecondBranch(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync($"This is second branch {Environment.NewLine}");
+    });
+}
+
+void FirstBranch(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync($"This is first branch {Environment.NewLine}");
+    });
 }
 
 app.UseHttpsRedirection();
